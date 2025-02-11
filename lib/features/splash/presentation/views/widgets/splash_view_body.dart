1d55903e095135242997b87712961f1cac6cf4b6
OmarMohamed <omarmohamed01284@gmail.com>
2025-02-11 15:01:48 +0200

    adding animation to splash view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gdg_camp_final_project/core/utils/app_colors.dart';
@@ -12,50 +13,70 @@ class SplashViewBody extends StatelessWidget {
         Positioned(
           top: -50,
           right: -128.r,
-          child: CircleAvatar(
-            radius: 128.r,
-            backgroundColor: AppColors.primaryColor.withValues(alpha: .67),
+          child: FadeInLeft(
+            duration: Duration(seconds: 2),
+            child: CircleAvatar(
+              radius: 128.r,
+              backgroundColor: AppColors.primaryColor.withValues(alpha: .67),
+            ),
           ),
         ),
         Positioned(
           top: 150,
           left: -21.5,
-          child: CircleAvatar(
-            radius: 21.5.r,
-            backgroundColor: AppColors.primaryColor.withValues(alpha: .67),
+          child: ShakeX(
+            from: 50,
+            duration: Duration(seconds: 2),
+            child: CircleAvatar(
+              radius: 21.5.r,
+              backgroundColor: AppColors.primaryColor.withValues(alpha: .67),
+            ),
           ),
         ),
         Positioned(
           top: 200,
           left: 30,
-          child: CircleAvatar(
-            radius: 29.r,
-            backgroundColor: AppColors.primaryColor.withValues(alpha: .2),
+          child: ShakeY(
+            from: 50,
+            duration: Duration(seconds: 2),
+            child: CircleAvatar(
+              radius: 29.r,
+              backgroundColor: AppColors.primaryColor.withValues(alpha: .2),
+            ),
           ),
         ),
         Center(child: Image.asset(AppImages.logoImage,width: 285.w,height: 100.h,fit: BoxFit.fill,)),
         Positioned(
           bottom: -80,
           left: -160.r,
-          child: CircleAvatar(
-            radius: 160.r,
-            backgroundColor: AppColors.primaryColor.withValues(alpha: .3),
+          child: FadeInRight(
+            duration: Duration(seconds: 2),
+            child: CircleAvatar(
+              radius: 160.r,
+              backgroundColor: AppColors.primaryColor.withValues(alpha: .3),
+            ),
           ),
         ),
         Positioned(
           bottom: 50,
           right: 70,
-          child: CircleAvatar(
-            radius: 21.5.r,
-            backgroundColor: AppColors.primaryColor.withValues(alpha: .67),
+          child: BounceInDown(
+            duration: Duration(seconds: 2),
+            child: CircleAvatar(
+              radius: 21.5.r,
+              backgroundColor: AppColors.primaryColor.withValues(alpha: .67),
+            ),
           ),
         ),
         Positioned(
           bottom: 120,
           right: 0,
-          child: CircleAvatar(
-            radius: 29.r,
-            backgroundColor: AppColors.primaryColor.withValues(alpha: .2),
+          child: Jello(
+            duration: Duration(seconds: 2),
+            child: CircleAvatar(
+              radius: 29.r,
+              backgroundColor: AppColors.primaryColor.withValues(alpha: .2),
+            ),
           ),
         ),
 
